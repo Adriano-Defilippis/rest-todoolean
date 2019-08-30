693a111f20a7fd3f0e2086958855af5a1c9008c7
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-08-30 18:30:07 +0200

    Cambiato Stile CSS

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function init(){
 
   getItems();
 
+  // Azione per aggiungere un item alla lista Su API
+  $('#new-to-do').click(postItem);
 
 
 };
@@ -20,16 +22,18 @@ function getItems(){
 
     success: function(data){
 
+      // Ciclo tutti i dati della lista per estrapolere i valori da dare ad Handlebars
       for (var i = 0; i < data.length; i++) {
-        var d = data[i];
 
+        var d = data[i];
+        // Funzione template di HANDLEBARS
         itemTemplate(
           {
            id: d.id,
            text: d.text
           }, target);
-      }
 
+      }
 
     },
     error: function(error){
@@ -38,6 +42,33 @@ function getItems(){
     }
   });
 
+}
+
+//Aggiungo un ToDoItem da chiamata POST
+function postItem(){
+
+  var newinput = $('#new-input').val();
+
+  $.ajax({
+
+    url: "http://157.230.17.132:3000/todos",
+    method: "POST",
+
+    data:{
+      text: newinput,
+    },
+
+    success: function(data){
+      //Ripulisto il container degli elementi in HTML
+      $('ul.container .box').remove();
+      //Richiamo la funzione per ottenere i nuovi dati dall'api
+      getItems();
+    },
+    error: function(error){
+      alert("Errore chiamata POST: ", error);
+      console.log("POST ITEM" ,error);
+    }
+  });
 }
 
 // Funzione per stampare tramite handlebars
